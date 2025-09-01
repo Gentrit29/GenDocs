d5875d457f5fb8db2663aeb23d2b50ec9e3cbba5
Gentrit <gentritzheg@gmail.com>
2025-09-01 13:48:08 +0200

    style(ui/go-back): update styling of GoBack component

--- src/ui/go-back.tsx
+++ src/ui/go-back.tsx
@@ -6,7 +6,7 @@ export default function GoBack({ path }: { path: string }) {
     <div id="tw-scope" className="mb-3">
       <Link
         to={path}
-        className="inline-flex items-center gap-2 rounded-full bg-gray-200 px-2 py-1 text-lg text-black transition-colors duration-300 ease-in hover:bg-green-500 lg:px-3 lg:py-1.5 dark:bg-neutral-800 dark:text-gray-200"
+        className="inline-flex items-center gap-2 rounded-3xl border border-gray-300 p-3 text-lg text-black transition-colors duration-300 ease-in hover:border-gray-400 dark:border-gray-500 dark:bg-neutral-800 dark:text-gray-300 dark:hover:border-gray-300"
       >
         <GoArrowLeft />
         Go back
