5dec39e8211c12490a45a727aa70a054aa3a68be
Gentrit <gentritzheg@gmail.com>
2025-09-01 13:46:05 +0200

    refactor(docs, ui): add margin to Callout; remove redundant titles in mdx files

--- src/ui/callout.tsx
+++ src/ui/callout.tsx
@@ -7,17 +7,18 @@ import {
 const styles = {
   note: {
     title: "Note",
-    style: "bg-blue-100 text-blue-900 border-l-4 border-blue-500 p-4",
+    style: "bg-blue-100 text-blue-900 border-l-4 border-blue-500 p-4 mb-6",
     icon: <HiOutlineInformationCircle className="h-6 w-6" />,
   },
   disclaimer: {
     title: "Disclaimer",
-    style: "bg-red-100 text-red-900 border-l-4 border-red-500 p-4",
+    style: "bg-red-100 text-red-900 border-l-4 border-red-500 p-4 mb-6",
     icon: <HiOutlineExclamationCircle className="h-6 w-6" />,
   },
   wip: {
     title: "Work In Progress",
-    style: "bg-yellow-100 text-yellow-900 border-l-4 border-yellow-500 p-4",
+    style:
+      "bg-yellow-100 text-yellow-900 border-l-4 border-yellow-500 p-4 mb-6",
     icon: <HiOutlineClock className="h-6 w-6" />,
   },
 };
